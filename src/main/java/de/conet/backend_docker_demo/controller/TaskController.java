7b2617c0c6a11d0df08ff782534fac3a13ee57df
Jamie Augustin <jamie.augustin@outlook.com>
2026-04-17 12:53:48 +0200

    Added lombok.config and changed PathVariable to Integer for testing

--- src/main/java/de/conet/backend_docker_demo/controller/TaskController.java
+++ src/main/java/de/conet/backend_docker_demo/controller/TaskController.java
@@ -21,12 +21,12 @@ public class TaskController {
     }
 
     @GetMapping("/getTasks/{id}")
-    public Task getTaskById(@RequestParam @PathVariable("id") Long id) {
+    public Task getTaskById(@PathVariable("id") Integer id) {
         return taskService.getTaskById(id);
     }
 
     @DeleteMapping("deleteTask/{id}")
-    public void deleteTaskById(@PathVariable("id") Long id) {
+    public void deleteTaskById(@PathVariable("id") Integer id) {
         taskService.deleteTaskById(id);
     }
 
